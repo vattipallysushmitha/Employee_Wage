34d1c77240e244e7023bb146732220c34ead114e
vattipallysushmitha <vengalreddysushmitha@gmail.com>
2022-07-10 16:25:00 +0530

    [sushmitha]month wages

--- empwage.sh
+++ empwage.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -x
-parttime=1
-fulltime=2
-RPH=20
-emp=$((RANDOM%3))
-        case $emp in
-                $fulltyme)
-                                emphrs=8
+parttym=1
+fulltym=2
+totalsalary=0
+emprph=20
+noworkingdays=20
+for(( day=1; day<=$noworkingdays; day++ ))
+        do
+                empcheck=$((RANDOM%2+1))
+                case $empcheck in
+                $fulltym) emphr=8
                                 ;;
-                $parttime )
-                                emphrs=4
-
+                $prttym) emprph=4
                                 ;;
-                         *)
-                                emphrs=0
-                                ;;
-        esac
-                salary=$(($emphrs*$RPH))
-                echo "salary" $salary
+                *) emphr=0
+                        ;;
+esac
+        salary=$(( $emphr + $emprph ))
+        totalsalary=$(( $totalsalary + $salary ))
+        done
+        echo "monthly salary is:" $totalsalary
 
 
